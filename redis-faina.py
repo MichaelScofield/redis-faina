0e0fde7e91f9af0f755be4c3fad8f4f854507ede
luofucong <bayinamine@gmail.com>
2016-12-21 14:49:57 +0800

    [FEATURE] Argument "with-port", aggregate keys by host and port. [REFACTOR] Remove command time statistics.

--- redis-faina.py
+++ redis-faina.py
@@ -6,7 +6,7 @@ import re
 import socket
 
 line_re_26 = re.compile(r"""
-    ^(?P<timestamp>[\d\.]+)\s\[(?P<db>\d+)\s(?P<ip>\d+\.\d+\.\d+\.\d+):\d+]\s"(?P<command>\w+)"(\s"(?P<key>[^(?<!\\)"]+)(?<!\\)")?(\s(?P<args>.+))?$
+    ^(?P<timestamp>[\d\.]+)\s\[(?P<db>\d+)\s(?P<ip>\d+\.\d+\.\d+\.\d+):(?P<port>\d+)]\s"(?P<command>\w+)"(\s"(?P<key>[^(?<!\\)"]+)(?<!\\)")?(\s(?P<args>.+))?$
     """, re.VERBOSE)
 
 # For Redisproxy.
@@ -14,45 +14,30 @@ line_re_99 = re.compile(r"""
     ^(?P<timestamp>[\d\.]+)\s\[/(?P<ip>\d+\.\d+\.\d+\.\d+):\d+]\s"(?P<command>\w+)"(\s"(?P<key>[^(?<!\\)"]+)(?<!\\)")?(\s(?P<args>.+))?$
     """, re.VERBOSE)
 
+
 class StatCounter(object):
-    def __init__(self, redis_version=2.6):
+    def __init__(self, redis_version=2.6, with_port=False):
         self.line_count = 0
         self.skipped_lines = 0
         self.commands = defaultdict(int)
         self.keys = defaultdict(int)
-        self.times = []
         self._cached_sorts = {}
-        self.start_ts = None
-        self.last_ts = None
-        self.last_entry = None
         self.redis_version = redis_version
         self.line_re = line_re_26 if self.redis_version == 2.6 else line_re_99
-
-    def _record_duration(self, entry):
-        ts = float(entry['timestamp']) * 1000 * 1000 # microseconds
-        if not self.start_ts:
-            self.start_ts = ts
-            self.last_ts = ts
-        duration = ts - self.last_ts
-        if self.redis_version < 2.5:
-            cur_entry = entry
-        else:
-            cur_entry = self.last_entry
-            self.last_entry = entry
-        if duration and cur_entry:
-            self.times.append((duration, cur_entry))
-        self.last_ts = ts
+        self.with_port = with_port
 
     def _record_command(self, entry):
         self.commands[entry['command']] += 1
 
-    def _record_key(self, key, ip):
+    def _record_key(self, key, ip, port):
         formatted_key = re.sub('\d{4,}', '#', key)
         try:
             (host, _, _) = socket.gethostbyaddr(ip)
             formatted_key = formatted_key + "@" + host
         except socket.herror:
             formatted_key = formatted_key + "@" + ip
+        if self.with_port:
+            formatted_key = formatted_key + ":" + port
         self.keys[formatted_key] += 1
 
     @staticmethod
@@ -67,7 +52,6 @@ class StatCounter(object):
             output += ' %s%s' % (' '.join(arg_parts[0:max_args_to_show]), ellipses)
         return output
 
-
     def _get_or_sort_list(self, ls):
         key = id(ls)
         if not key in self._cached_sorts:
@@ -75,46 +59,16 @@ class StatCounter(object):
             self._cached_sorts[key] = sorted_items
         return self._cached_sorts[key]
 
-    def _time_stats(self, times):
-        sorted_times = self._get_or_sort_list(times)
-        num_times = len(sorted_times)
-        percent_50 = sorted_times[int(num_times / 2)][0]
-        percent_75 = sorted_times[int(num_times * .75)][0]
-        percent_90 = sorted_times[int(num_times * .90)][0]
-        percent_99 = sorted_times[int(num_times * .99)][0]
-        return (("Median", percent_50),
-                ("75%", percent_75),
-                ("90%", percent_90),
-                ("99%", percent_99))
-
-    def _heaviest_commands(self, times):
-        times_by_command = defaultdict(int)
-        for time, entry in times:
-            times_by_command[entry['command']] += time
-        return self._top_n(times_by_command)
-
-    def _slowest_commands(self, times, n=8):
-        sorted_times = self._get_or_sort_list(times)
-        slowest_commands = reversed(sorted_times[-n:])
-        printable_commands = [(str(time), self._reformat_entry(entry)) \
-                              for time, entry in slowest_commands]
-        return printable_commands
-
     def _general_stats(self):
-        total_time = (self.last_ts - self.start_ts) / (1000*1000)
-        return (
-            ("Lines Processed", self.line_count),
-            ("Commands/Sec", '%.2f' % (self.line_count / total_time))
-        )
+        return [("Lines Processed", self.line_count)]
 
     def process_entry(self, entry):
-        self._record_duration(entry)
         self._record_command(entry)
         if entry['key']:
-            self._record_key(entry['key'], entry['ip'])
+            self._record_key(entry['key'], entry['ip'], entry['port'])
 
     def _top_n(self, stat):
-        sorted_items = sorted(stat.iteritems(), key = lambda x: x[1], reverse = True)
+        sorted_items = sorted(stat.iteritems(), key=lambda x: x[1], reverse=True)
         return sorted_items[:]
 
     def _pretty_print(self, result, title, percentages=False):
@@ -131,17 +85,13 @@ class StatCounter(object):
             if percentages:
                 val_padding = max(max_val_len - len(str(val)), 0) * ' '
                 val = '%s%s\t(%.2f%%)' % (val, val_padding, (float(val) / self.line_count) * 100)
-            print key,key_padding,'\t',val
+            print key, key_padding, '\t', val
         print
-
 
     def print_stats(self):
         self._pretty_print(self._general_stats(), 'Overall Stats')
-        self._pretty_print(self._top_n(self.keys), 'Top Keys', percentages = True)
-        self._pretty_print(self._top_n(self.commands), 'Top Commands', percentages = True)
-        self._pretty_print(self._time_stats(self.times), 'Command Time (microsecs)')
-        self._pretty_print(self._heaviest_commands(self.times), 'Heaviest Commands (microsecs)')
-        self._pretty_print(self._slowest_commands(self.times), 'Slowest Calls')
+        self._pretty_print(self._top_n(self.keys), 'Top Keys', percentages=True)
+        self._pretty_print(self._top_n(self.commands), 'Top Commands', percentages=True)
 
     def process_input(self, input):
         for line in input:
@@ -154,21 +104,28 @@ class StatCounter(object):
                 continue
             self.process_entry(match.groupdict())
 
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
         'input',
-        type = argparse.FileType('r'),
-        default = sys.stdin,
-        nargs = '?',
-        help = "File to parse; will read from stdin otherwise")
+        type=argparse.FileType('r'),
+        default=sys.stdin,
+        nargs='?',
+        help="File to parse; will read from stdin otherwise")
     parser.add_argument(
         '--redis-version',
-        type = float,
-        default = 2.6,
-        help = "Version of the redis server being monitored",
-        required = False)
+        type=float,
+        default=2.6,
+        help="Version of the redis server being monitored",
+        required=False)
+    parser.add_argument(
+        '--with-port',
+        type=bool,
+        default=False,
+        help="Aggregate keys by host and port, or solely by host.",
+        required=False)
     args = parser.parse_args()
-    counter = StatCounter(redis_version=args.redis_version)
+    counter = StatCounter(redis_version=args.redis_version, with_port=args.with_port)
     counter.process_input(args.input)
     counter.print_stats()
